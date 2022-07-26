9abe78095243164cfa00c2001d5b5b3f1dc9368b
zndelfin <zndelfin.dev@gmail.com>
2022-07-26 09:49:00 -0400

    Show ID vs using IDas Key value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,19 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contacts){
+  return (
+    <Card
+      id = {contacts.id} //custom property tap into ID
+      key = {contacts.id} //hidden and cannot be used
+      name = {contacts.name}
+      img = {contacts.imgURL}
+      tel = {contacts.phone}
+      email = {contacts.email}
+    />
+  );
+  
+}
 
 
 function App() {
@@ -11,8 +24,9 @@ function App() {
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaDufRmY_UGMMWynBmxOU6n3wfNctXApubmw&usqp=CAU" />
  
-      
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -29,7 +43,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      /> 
+      />  */}
     </div>
   );
 }
