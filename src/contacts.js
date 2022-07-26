67100d2704021c1d637169e1daeec3919c1c5a49
zndelfin <zndelfin.dev@gmail.com>
2022-07-26 09:02:03 -0400

    Initial Commit of Contacts App

---
+++ src/contacts.js
@@ -0,0 +1,28 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Lee Jieun",
+    imgURL:
+      "https://i.scdn.co/image/ab6761610000e5eb006ff3c0136a71bfb9928d34",
+    phone: "+123 456 789",
+    email: "iu@loen.com"
+  },
+  {
+    id: 2,
+    name: "Chris Evans",
+    imgURL:
+      "https://helios-i.mashable.com/imagery/articles/07EYhaHUGB6ozD3GtOXk3N3/hero-image.fill.size_1200x1200.v1654795267.png",
+    phone: "+987 654 321",
+    email: "chris@evans.com"
+  },
+  {
+    id: 3,
+    name: "Marc Marquez",
+    imgURL:
+      "https://cdn.shopify.com/s/files/1/1223/6434/files/profile.jpg?v=1580366119",
+    phone: "+918 372 574",
+    email: "mm93@motogp.com"
+  }
+];
+
+export default contacts;
